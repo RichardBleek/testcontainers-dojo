99cb6011ca0d5b7dc0c3df33edee63d323c7753e
Richard Bleek <richardbleek@gmail.com>
2021-02-22 09:45:41 +0100

    solution-3

--- src/test/java/com/codequad/testcontainerdojo/PizzaRepositoryTest.java
+++ src/test/java/com/codequad/testcontainerdojo/PizzaRepositoryTest.java
@@ -3,11 +3,33 @@ package com.codequad.testcontainerdojo;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.utility.DockerImageName;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class PizzaRepositoryTest {
+
+    private static final PostgreSQLContainer<?> POSTGRESQL_CONTAINER = new PostgreSQLContainer<>(
+            DockerImageName.parse("postgres").withTag("alpine"))
+            .withDatabaseName("dojo")
+            .withUsername("postgres")
+            .withPassword("mysecretpassword")
+            .withReuse(false);
+
+    static {
+        POSTGRESQL_CONTAINER.start();
+    }
+
+    @DynamicPropertySource
+    static void containerProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", POSTGRESQL_CONTAINER::getJdbcUrl);
+        registry.add("spring.datasource.username", POSTGRESQL_CONTAINER::getUsername);
+        registry.add("spring.datasource.password", POSTGRESQL_CONTAINER::getPassword);
+    }
 
     @Autowired
     PizzaRepository pizzaRepository;
