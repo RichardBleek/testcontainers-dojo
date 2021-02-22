6f9f77144b0cc8d75c2df83a42b2aeea72b0b3f4
Richard Bleek <richardbleek@gmail.com>
2021-02-22 09:39:22 +0100

    solution-1

--- src/test/java/com/codequad/testcontainerdojo/PizzaRepositoryTest.java
+++ src/test/java/com/codequad/testcontainerdojo/PizzaRepositoryTest.java
@@ -16,9 +16,11 @@ class PizzaRepositoryTest {
     void testInsertPizza() {
         Pizza margherita = Pizza.builder().name("Margherita").price(800).build();
         Pizza caprese = Pizza.builder().name("Caprese").price(900).build();
+        Pizza hawaii = Pizza.builder().name("Hawaii").price(900).build();
 
         pizzaRepository.save(margherita);
         pizzaRepository.save(caprese);
+        pizzaRepository.save(hawaii);
 
         assertEquals(pizzaRepository.findAll().size(), 3);
     }
